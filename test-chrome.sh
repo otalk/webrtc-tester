a419819e0b4f4cb5aec2cb4c2dff644d18bf02cb
Philipp Hancke <fippo@andyet.net>
2014-09-23 20:58:43 +0200

    import code

---
+++ test-chrome.sh
@@ -0,0 +1,70 @@
+#!/bin/bash -e
+# Copyright © 2014 &yet
+#
+# based on https://github.com/GoogleChrome/webrtc/blob/master/samples/web/content/apprtc/turn-prober/turn-prober.sh
+# Copyright (c) 2014, The WebRTC project authors. All rights reserved.
+# see https://github.com/GoogleChrome/webrtc/blob/master/LICENSE
+
+DISPLAY=
+CHROME="google-chrome"
+CHROME_ARGS="--use-fake-ui-for-media-stream --use-fake-device-for-media-stream"
+HOST=$1
+ROOM=$2
+URL=https://${HOST}/${ROOM}
+
+function chrome_pids() {
+  ps axuwww|grep $D|grep c[h]rome|awk '{print $2}'
+}
+
+cd $(dirname $0)
+export D=$(mktemp -d)
+#"https_${HOST}_0.localstorage"
+
+# prefill localstorage
+LOCALSTORAGE_DIR="${D}/Default/Local Storage/"
+mkdir -p "${LOCALSTORAGE_DIR}"
+cp data/chrome-localstorage-template "${LOCALSTORAGE_DIR}/https_${HOST}_0.localstorage"
+
+CHROME_LOG_FILE="${D}/chrome_debug.log"
+touch $CHROME_LOG_FILE
+
+XVFB="xvfb-run -a -e $CHROME_LOG_FILE -s '-screen 0 1024x768x24'"
+if [ -n "$DISPLAY" ]; then
+  XVFB=""
+fi
+
+# "eval" below is required by $XVFB containing a quoted argument.
+eval $XVFB $CHROME \
+  --enable-logging=stderr \
+  --no-first-run \
+  --user-data-dir=$D \
+  ${CHROME_ARGS} \
+  --vmodule="*media/*=3,*turn*=3" \
+  "${URL}" > $CHROME_LOG_FILE 2>&1 &
+CHROME_PID=$!
+#  --disable-web-security \
+
+while ! grep -q "P2P connected" $CHROME_LOG_FILE && chrome_pids|grep -q .; do
+  sleep 0.1
+done
+# wait for the peer to notice
+sleep 5
+
+# Suppress bash's Killed message for the chrome above.
+exec 3>&2
+exec 2>/dev/null
+while [ ! -z "$(chrome_pids)" ]; do
+  kill -9 $(chrome_pids)
+done
+exec 2>&3
+exec 3>&-
+
+DONE=$(grep "P2P connected" $CHROME_LOG_FILE)
+EXIT_CODE=0
+if ! grep -q "P2P connected" $CHROME_LOG_FILE; then
+  cat $CHROME_LOG_FILE
+  EXIT_CODE=1
+fi
+
+rm -rf $D
+exit $EXIT_CODE
---
+++ test-runner.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+TIMEOUT="60"
+HOST="beta.talky.io"
+ROOM="automattedtesting_${RANDOM}"
+
+
+# chrome #1
+( ./test-chrome.sh $HOST "${ROOM}" >> log1.log 2>&1 ) &
+pidwatch=$!
+
+# chrome #2
+( ./test-chrome.sh $HOST "${ROOM}" >> log2.log 2>&1 ) &
+pidwatch2=$!
+
+# now give them some time to connect
+
+# this timeout is for the overall test process
+( sleep ${TIMEOUT} ; kill $pidwatch > /dev/null 2>&1 ; kill $pidwatch2 > /dev/null 2>&1 ; echo "timedout" ) &
+pidwatcher=$!
+
+echo "${pidwatch} ${pidwatch2} ${pidwatcher}"
+
+if wait $pidwatch2 ; then
+  echo "--- finished" #                 | tee -a ~/test_run.log
+else
+  echo "--- stopped" #                  | tee -a ~/test_run.log
+fi
+
+pkill -HUP -P $pidwatcher
