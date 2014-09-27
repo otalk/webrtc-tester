1b62754c76db699cdfdd7440c8a1b44d13ca8369
Philipp Hancke <fippo@andyet.net>
2014-09-27 20:14:29 +0200

    be silent in case nothing happens, cat log if bad things happen

--- README.md
+++ README.md
@@ -6,13 +6,12 @@ WebRTC Deployment Testing Toolkit
 #Required software
 * xvfb
 * sqlite3
-* pip
+* python-pip
 * firefox
   * mozrunner (via pip/easy\_install)
 
 #Recommended reading
-Google has published two excellent blog posts on automatted interop testing between 
-Firefox and Chrome:
+The webrtc team has published two excellent blog posts on automatted interop testing between Firefox and Chrome:
 * [Part 1](http://googletesting.blogspot.se/2014/08/chrome-firefox-webrtc-interop-test-pt-1.html)
 * [Part 2](http://googletesting.blogspot.se/2014/09/chrome-firefox-webrtc-interop-test-pt-2.html)
 
--- test-runner.sh
+++ test-runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TIMEOUT="60"
+TIMEOUT="90"
 DISPLAY=
 HOST="beta.talky.io"
 ROOM="automatedtesting_${RANDOM}"
@@ -14,13 +14,18 @@ function cleanup() {
   function xvfb_pids() {
     ps x -o "%p %r %c" | grep Xvfb | grep $$ | awk '{print $1}'
   }
+  exec 3>&2
+  exec 2>/dev/null
   while [ ! -z "$(xvfb_pids)" ]; do
     kill $(xvfb_pids)
   done
   pkill -HUP -P $pidwatch
   pkill -HUP -P $pidwatch2
+  exec 2>&3
+  exec 3>&-
 }
 trap cleanup EXIT
+
 # this timeout is for the overall test process
 ( sleep ${TIMEOUT} ) &
 pidwatcher=$!
@@ -30,14 +35,14 @@ pidwatcher=$!
 pidwatch=$!
  
 # browser #2
-( ./test-browser.sh firefox $HOST "${ROOM}" "${COND}" >> log2.log 2>&1 ; kill $pidwatcher 2> /dev/null ) 2>/dev/null &
+( ./test-browser.sh chromium-browser $HOST "${ROOM}" "${COND}" >> log2.log 2>&1 ; kill $pidwatcher 2> /dev/null ) 2>/dev/null &
 pidwatch2=$!
  
-echo "${pidwatcher} watching ${pidwatch} ${pidwatch2}"
+#echo "${pidwatcher} watching ${pidwatch} ${pidwatch2}"
  
 if wait $pidwatcher 2>/dev/null; then
   echo "--- timedout"
-else
-  echo "--- success"
+  cat log1.log 
+  cat log2.log
 fi
- 
+# do nothing in the case of success
